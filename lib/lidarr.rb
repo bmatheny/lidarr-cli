144578e09d173ca364369eb2d5da7554080960c3
Blake Matheny <bmatheny@mobocracy.net>
2022-10-16 09:21:52 -0500

    Initial repo

---
+++ lib/lidarr.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lidarr/version"
+
+module Lidarr
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/lidarr_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Lidarr do
+  it "has a version number" do
+    expect(Lidarr::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
